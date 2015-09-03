dad39721e7829b8ceb2ae6391d723b9058d668a9
ichizok <gclient.gaap@gmail.com>
2015-09-03 21:28:26 +0900

    vim74: 7.4.854

--- vim74.rb
+++ vim74.rb
@@ -3,7 +3,7 @@ class Vim74 < Formula
   homepage "http://www.vim.org/"
   # Get stable versions from hg repo instead of downloading an increasing
   # number of separate patches.
-  patchlevel = 843
+  patchlevel = 854
   url "https://github.com/vim/vim.git", :tag => format("v7.4.%03d", patchlevel)
   version "7.4.#{patchlevel}"
 
