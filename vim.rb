2bc5f5f55d4d0f89d47417d82e5678b0edac4c32
ichizok <gclient.gaap@gmail.com>
2016-02-14 00:05:36 +0900

    vim: 7.4.1309

--- vim.rb
+++ vim.rb
@@ -3,7 +3,7 @@ class Vim < Formula
   homepage "http://www.vim.org/"
   # Get stable versions from hg repo instead of downloading an increasing
   # number of separate patches.
-  patchlevel = 1236
+  patchlevel = 1309
   url "https://github.com/vim/vim.git", :tag => format("v7.4.%03d", patchlevel)
   version "7.4.#{patchlevel}"
 
