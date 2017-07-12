7ab28bed5db9d2b2605994614ea0e714ff622a96
ichizok <gclient.gaap@gmail.com>
2017-07-12 10:43:56 +0900

    vim: 8.0.709

--- vim.rb
+++ vim.rb
@@ -1,7 +1,7 @@
 class Vim < Formula
   desc "Vi \"workalike\" with many additional features"
   homepage "http://www.vim.org/"
-  patchlevel = 706
+  patchlevel = 709
   url "https://github.com/vim/vim.git", :tag => format("v8.0.%04d", patchlevel)
   version "8.0.#{patchlevel}"
 
