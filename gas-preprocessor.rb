ef6089d2b76a788dfff12efd80667b5cd464c923
ichizok <gclient.gaap@gmail.com>
2018-04-09 18:35:04 +0900

    gas-preprocessor: ScriptFileFormula is deprecated

--- gas-preprocessor.rb
+++ gas-preprocessor.rb
@@ -1,4 +1,4 @@
-class GasPreprocessor < ScriptFileFormula
+class GasPreprocessor < Formula
   homepage "https://github.com/yuvi/gas-preprocessor"
   url "https://raw.github.com/yuvi/gas-preprocessor/master/gas-preprocessor.pl"
   sha256 "554b8860ca85f90a6c268a709790ebfc37a16aa99ee235aca5cb95deed8275b3"
