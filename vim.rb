c5b44b2b6105b36733afe4f615db5b5b66643c33
ichizok <gclient.gaap@gmail.com>
2020-07-23 00:26:59 +0900

    vim: 8.2.1266

--- vim.rb
+++ vim.rb
@@ -1,7 +1,7 @@
 class Vim < Formula
   desc "Vi 'workalike' with many additional features"
   homepage "https://www.vim.org/"
-  patchlevel = 1258
+  patchlevel = 1266
   url "https://github.com/vim/vim.git", :tag => format("v8.2.%04d", patchlevel)
   head "https://github.com/vim/vim.git"
 
