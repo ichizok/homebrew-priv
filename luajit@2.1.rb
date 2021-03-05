727d2e14d65c567b897d4528755106b0d2ee5eb7
ichizok <gclient.gaap@gmail.com>
2021-03-06 07:50:44 +0900

    add luajit@2.1

---
+++ luajit@2.1.rb
@@ -0,0 +1,54 @@
+class LuajitAT21 < Formula
+  desc "Just-In-Time Compiler (JIT) for the Lua programming language"
+  homepage "https://luajit.org/luajit.html"
+  license "MIT"
+  head "https://github.com/openresty/luajit2.git", branch: "v2.1-agentzh"
+
+  def install
+    # 1 - Override the hardcoded gcc.
+    # 2 - Remove the "-march=i686" so we can set the march in cflags.
+    # Both changes should persist and were discussed upstream.
+    inreplace "src/Makefile" do |f|
+      f.change_make_var! "CC", ENV.cc
+      f.change_make_var! "CCOPT_x86", ""
+    end
+
+    # Per https://luajit.org/install.html: If MACOSX_DEPLOYMENT_TARGET
+    # is not set then it's forced to 10.4, which breaks compile on Mojave.
+    ENV["MACOSX_DEPLOYMENT_TARGET"] = MacOS.version
+
+    args = %W[PREFIX=#{prefix}]
+
+    system "make", "amalg", *args
+    system "make", "install", *args
+
+    # Unstable branch doesn't install symlink for luajit.
+    # This breaks tools like `luarock` who requires the `luajit` bin to be present.
+    bin.install_symlink Dir[bin/"luajit-*"].first => "luajit"
+
+    # LuaJIT doesn't automatically symlink unversioned libraries:
+    # https://github.com/Homebrew/homebrew/issues/45854.
+    lib.install_symlink lib/"libluajit-5.1.dylib" => "libluajit.dylib"
+    lib.install_symlink lib/"libluajit-5.1.a" => "libluajit.a"
+
+    # Fix path in pkg-config so modules are installed
+    # to permanent location rather than inside the Cellar.
+    inreplace lib/"pkgconfig/luajit.pc" do |s|
+      s.gsub! "INSTALL_LMOD=${prefix}/share/lua/${abiver}",
+              "INSTALL_LMOD=#{HOMEBREW_PREFIX}/share/lua/${abiver}"
+      s.gsub! "INSTALL_CMOD=${prefix}/${multilib}/lua/${abiver}",
+              "INSTALL_CMOD=#{HOMEBREW_PREFIX}/${multilib}/lua/${abiver}"
+    end
+
+    # Having an empty Lua dir in lib/share can mess with other Homebrew Luas.
+    %W[#{lib}/lua #{share}/lua].each { |d| rm_rf d }
+  end
+
+  test do
+    system "#{bin}/luajit", "-e", <<~EOS
+      local ffi = require("ffi")
+      ffi.cdef("int printf(const char *fmt, ...);")
+      ffi.C.printf("Hello %s!\\n", "#{ENV["USER"]}")
+    EOS
+  end
+end
