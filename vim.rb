dcc9ae27d4c8dcd9684d54ae51527c4090b1319e
ichizok <gclient.gaap@gmail.com>
2023-03-28 12:40:18 +0900

    vim: change dependency from python@3.10 to python

--- vim.rb
+++ vim.rb
@@ -6,7 +6,7 @@ class Vim < Formula
 
   depends_on "luajit"
   depends_on "perl"
-  depends_on "python@3.10"
+  depends_on "python"
   depends_on "ruby"
 
   conflicts_with "ex-vi",
@@ -16,7 +16,7 @@ class Vim < Formula
     because: "vim and macvim both install vi* binaries"
 
   def install
-    ENV.prepend_path "PATH", Formula["python@3.10"].opt_libexec/"bin"
+    ENV.prepend_path "PATH", Formula["python"].opt_libexec/"bin"
 
     # https://github.com/Homebrew/homebrew-core/pull/1046
     ENV.delete("SDKROOT")
